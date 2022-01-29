c263c14b4fc66fdb7cea2874516938512dbf42c5
EDDvLyon <98643280+EDDvLyon@users.noreply.github.com>
2022-01-29 12:57:34 +0200

    Decode https://isengard.market/nft/MISETRY1-767331-01 based on hints @ https://isengard.market/collection/MISETRY1-76ead1
    Decrypt image encrypted with AES256. 
    Encrypted image: https://isengard.market/nft/MISETRY1-767331-01 
    Puzzle/Hints for obtaining the Seed & Key: https://isengard.market/collection/MISETRY1-76ead1

---
+++ PicCrypto/PicCrypto/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace PicCrypto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cryptoImgToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip3 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dasdasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dadsaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dasdaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.misteryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.decryptoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imgToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.contextMenuStrip3.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(904, 503);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cryptoImgToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 28);
+            // 
+            // cryptoImgToolStripMenuItem
+            // 
+            this.cryptoImgToolStripMenuItem.Name = "cryptoImgToolStripMenuItem";
+            this.cryptoImgToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.cryptoImgToolStripMenuItem.Text = "Crypto Img";
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dadToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(105, 28);
+            // 
+            // dadToolStripMenuItem
+            // 
+            this.dadToolStripMenuItem.Name = "dadToolStripMenuItem";
+            this.dadToolStripMenuItem.Size = new System.Drawing.Size(104, 24);
+            this.dadToolStripMenuItem.Text = "dad";
+            // 
+            // contextMenuStrip3
+            // 
+            this.contextMenuStrip3.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dasdasToolStripMenuItem,
+            this.dasdaToolStripMenuItem});
+            this.contextMenuStrip3.Name = "contextMenuStrip3";
+            this.contextMenuStrip3.Size = new System.Drawing.Size(125, 52);
+            // 
+            // dasdasToolStripMenuItem
+            // 
+            this.dasdasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dadsaToolStripMenuItem});
+            this.dasdasToolStripMenuItem.Name = "dasdasToolStripMenuItem";
+            this.dasdasToolStripMenuItem.Size = new System.Drawing.Size(124, 24);
+            this.dasdasToolStripMenuItem.Text = "dasdas";
+            // 
+            // dadsaToolStripMenuItem
+            // 
+            this.dadsaToolStripMenuItem.Name = "dadsaToolStripMenuItem";
+            this.dadsaToolStripMenuItem.Size = new System.Drawing.Size(132, 26);
+            this.dadsaToolStripMenuItem.Text = "dadsa";
+            // 
+            // dasdaToolStripMenuItem
+            // 
+            this.dasdaToolStripMenuItem.Name = "dasdaToolStripMenuItem";
+            this.dasdaToolStripMenuItem.Size = new System.Drawing.Size(124, 24);
+            this.dasdaToolStripMenuItem.Text = "dasda";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.misteryToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.MinimumSize = new System.Drawing.Size(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(928, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // misteryToolStripMenuItem
+            // 
+            this.misteryToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.decryptoToolStripMenuItem});
+            this.misteryToolStripMenuItem.Name = "misteryToolStripMenuItem";
+            this.misteryToolStripMenuItem.Size = new System.Drawing.Size(71, 26);
+            this.misteryToolStripMenuItem.Text = "Mistery";
+            // 
+            // decryptoToolStripMenuItem
+            // 
+            this.decryptoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.imgToolStripMenuItem1});
+            this.decryptoToolStripMenuItem.Name = "decryptoToolStripMenuItem";
+            this.decryptoToolStripMenuItem.Size = new System.Drawing.Size(153, 26);
+            this.decryptoToolStripMenuItem.Text = "Decrypto";
+            // 
+            // imgToolStripMenuItem1
+            // 
+            this.imgToolStripMenuItem1.Name = "imgToolStripMenuItem1";
+            this.imgToolStripMenuItem1.Size = new System.Drawing.Size(118, 26);
+            this.imgToolStripMenuItem1.Text = "Img";
+            this.imgToolStripMenuItem1.Click += new System.EventHandler(this.imgToolStripMenuItem1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(196, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(446, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Seed:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(407, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Key:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 549);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "CryptoPic";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.contextMenuStrip3.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cryptoImgToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem dadToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip3;
+        private System.Windows.Forms.ToolStripMenuItem dasdasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dadsaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dasdaToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem misteryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem decryptoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imgToolStripMenuItem1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PicCrypto/PicCrypto/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace PicCrypto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Boolean encry = false, decrypt = false;
+
+        Bitmap myImg;
+        List<byte[]> myRed = new List<byte[]>();
+        List<byte[]> myGreen = new List<byte[]>();
+        List<byte[]> myBlue = new List<byte[]>();
+
+        int OrigImgW = 0;
+        int OrigImgH = 0;
+
+        byte[] mySeed = new byte[16];
+        byte[] myPass = new byte[16];
+
+        Boolean decrypt = false;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (false == decrypt)
+            {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "image files|*.jpg;*.png;*.gif;*bmp;";
+            DialogResult dr = ofd.ShowDialog();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            if (dr == DialogResult.OK)
+            {
+                myImg = new Bitmap(ofd.FileName);
+
+                OrigImgW = myImg.Width;
+                OrigImgH = myImg.Height;
+                float ratio = (float)OrigImgW / OrigImgH;
+
+                this.Width = (int)(ratio * this.Height);
+
+
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+
+                int size = myImg.Width * myImg.Height;
+                if ((size % 16) != 0)
+                {
+                    size = ((size / 16) + 1) * 16;
+                }
+                byte[] dataRed = new byte[size];
+                byte[] dataGreen = new byte[size];
+                byte[] dataBlue = new byte[size];
+
+                int cnt = 0;
+                for (int i = 0; i < myImg.Width; i++)
+                {
+                    for (int j = 0; j < myImg.Height; j++)
+                    {
+                        dataRed[cnt] = myImg.GetPixel(i, j).R;
+                        dataGreen[cnt] = myImg.GetPixel(i, j).G;
+                        dataBlue[cnt] = myImg.GetPixel(i, j).B;
+                        cnt++;
+                    }
+                }
+
+                myRed.Add(dataRed);
+                myGreen.Add(dataGreen);
+                myBlue.Add(dataBlue);
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+                pictureBox2.Visible = true;
+                pictureBox1.Visible = false;
+            }
+        
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            this.Width = 500;
+            this.Height = 600;
+
+            //pictureBox1.Size = Form1.
+            int w = this.Size.Width - 35;
+            int h = this.Size.Height - 70;
+
+            pictureBox1.Size = new Size(w, h);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int w = this.Size.Width - 35;
+            int h = this.Size.Height - 70;
+
+            pictureBox1.Size = new Size(w, h);
+        }
+
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+            pictureBox1.Visible = true;
+        }
+
+        private void imgToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+            int length = myRed[0].Length;
+            GetSeed();
+
+            var rfc = new Rfc2898DeriveBytes(textBox2.Text, mySeed);
+            byte[] Key = rfc.GetBytes(16);
+            byte[] IV = rfc.GetBytes(16);
+
+            byte[] decryptedR, decryptedG, decryptedB;
+
+            using (MemoryStream mStream = new MemoryStream(myRed[0]))
+            {
+                using (AesCryptoServiceProvider aesProvider = new AesCryptoServiceProvider())
+                {
+                    aesProvider.Padding = PaddingMode.None;
+                    using (CryptoStream cryptoStream = new CryptoStream(mStream, aesProvider.CreateDecryptor(Key, IV), CryptoStreamMode.Read))
+                    {
+                        cryptoStream.Read(myRed[0], 0, length);
+                    }
+                }
+                decryptedR = mStream.ToArray().Take(length).ToArray();
+            }
+
+            using (MemoryStream mStream = new MemoryStream(myGreen[0]))
+            {
+                using (AesCryptoServiceProvider aesProvider = new AesCryptoServiceProvider())
+                {
+                    aesProvider.Padding = PaddingMode.None;
+                    using (CryptoStream cryptoStream = new CryptoStream(mStream, aesProvider.CreateDecryptor(Key, IV), CryptoStreamMode.Read))
+                    {
+                        cryptoStream.Read(myGreen[0], 0, length);
+                    }
+                }
+                decryptedG = mStream.ToArray().Take(length).ToArray();
+            }
+
+            using (MemoryStream mStream = new MemoryStream(myBlue[0]))
+            {
+                using (AesCryptoServiceProvider aesProvider = new AesCryptoServiceProvider())
+                {
+                    aesProvider.Padding = PaddingMode.None;
+                    using (CryptoStream cryptoStream = new CryptoStream(mStream, aesProvider.CreateDecryptor(Key, IV), CryptoStreamMode.Read))
+                    {
+                        cryptoStream.Read(myBlue[0], 0, length);
+                    }
+                }
+                decryptedB = mStream.ToArray().Take(length).ToArray();
+            }
+
+            Bitmap encodedBmp = new Bitmap(OrigImgW, OrigImgH);
+            pictureBox2.Size = new Size(pictureBox1.Width, pictureBox1.Height);
+            int cnt = 0;
+
+            for (int i = 0; i < OrigImgW; i++)
+            {
+                for (int j = 0; j < OrigImgH; j++)
+                {
+                    Color mycolor = Color.FromArgb(decryptedR[cnt], decryptedG[cnt], decryptedB[cnt]);
+                    encodedBmp.SetPixel(i, j, mycolor);
+
+                    cnt++;
+                }
+            }
+
+            decrypt = true;
+
+            pictureBox2.Image = encodedBmp;
+            pictureBox2.Visible = true;
+            pictureBox1.Visible = false;
+        }
+
+        void GetSeed()
+        {
+            for(int i = 0; i < textBox1.Text.Length; i++)
+            {
+                mySeed[i]  = Convert.ToByte(Convert.ToChar(textBox1.Text.Substring(i,1)));
+            }
+        }
+
+        void GetPassword()
+        {
+            for (int i = 0; i < textBox2.Text.Length; i++)
+            {
+                myPass[i] = Convert.ToByte(textBox2.Text.Substring(i, 1));
+            }
+        }
+    }
+}
